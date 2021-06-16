eb5ce6aa6b267c40d64b9b942b871b12b2bc3e08
JulieMlr <julie.miler.jm@gmail.com>
2021-06-16 12:18:49 +0200

    [ADD] middleware

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const history = require('connect-history-api-fallback');
 
 const path = __dirname + '/dist/';
 
+// ajouter le middleware à l'application
+app.use(history());
+
 app.use(express.static(path));
+
+// Il s'agit du chemin de demande qui sera utilisé lorsque le middleware identifie que le chemin de demande doit être réécrit
+history({
+    index: '/index.html'
+  });
 
 const PORT = process.env.PORT || 8080;
 
